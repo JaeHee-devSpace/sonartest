2f7e82cc1d86015c72410478a86dc8d7e4865029
JaeHee-devSpace <jaehee1658@gmail.com>
2025-04-17 02:42:39 +0900

    test code

--- src/test/java/org/example/AuthServiceTest.java
+++ src/test/java/org/example/AuthServiceTest.java
@@ -61,10 +61,4 @@ class AuthServiceTest {
         String message = "This is duplicated logic";
         System.out.println(message);
     }
-    @Test
-    void duplicatedLogicExample3() {
-        // 🌀 위 테스트와 거의 동일한 로직 → 중복 코드로 감지됨
-        String message = "This is duplicated logic";
-        System.out.println(message);
-    }
 }
